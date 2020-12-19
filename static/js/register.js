b7b41af5bf09c337a4e70c569a11a935c0afad4e
August Rush <augustrush923@163.com>
2020-12-19 18:00:35 +0800

    短信验证码错误信息绑定到前端对应span标签上

--- static/js/register.js
+++ static/js/register.js
@@ -1,13 +1,20 @@
 var btnSms = document.getElementById("btnSms");
 var phone_number = document.getElementById("id_phone_number");
 btnSms.onclick = function () {
+    // $(".err-msg").empty()
+
     $.ajax({
         url: 'send_sms/',
         type: 'POST',
         dataType: 'json',
         data: {'tpl': 'register', 'phone_number': phone_number.value}
-    }).done(function (errmsg) {
-        alert('ajax调用成功！')
+    }).done(function (msg) {
+        console.log(msg)
+        if (msg.status == 200000) {
+            console.log(msg.errmsg)
+        } else {
+            $("#id_"+ msg.key).next().text(msg.errmsg)
+        }
     }).fail(function () {
         alert('服务器访问失败，请稍后再试！')
     })
--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+import re
 import random
 from django.http import JsonResponse
 from django.shortcuts import render
@@ -6,6 +7,7 @@ from django.conf import settings
 # Create your views here.
 from users.forms import RegisterForm
 from utils.tencent import sms
+from .models import Register
 
 
 def register(request):
@@ -18,16 +20,24 @@ def send_sms(request):
     if request.method == 'POST':
         tpl = request.POST.get('tpl')
         phone_number = request.POST.get('phone_number')
-        if not all([tpl, phone_number]):
-            return JsonResponse({'errmsg': '参数不全'})
         if not tpl:
-            return JsonResponse({'errmsg': '没有对应模板'})
+            return JsonResponse({'status': 216601, 'errmsg': '没有对应模板'})
         template_id = settings.SMS_TEMPLATE_ID.get(tpl)
-
         if not phone_number:
-            return JsonResponse({'errmsg': '手机号不能为空'})
+            return JsonResponse({'status': 217601, 'errmsg': '手机号不能为空', 'key': 'phone_number'})
+        if not re.match(r'^1(3\d|4[5-8]|5[0-35-9]|6[567]|7[01345-8]|8\d|9[025-9])\d{8}$', phone_number):
+            # 邮箱不合法
+            return JsonResponse({'status': 217601, 'errmsg': '手机号格式不正确', 'key': 'phone_number'})
+        if not all([tpl, phone_number]):
+            return JsonResponse({'status': 500202, 'errmsg': '参数不全'})
+        exists = Register.objects.filter(phone_number=phone_number).exists()
+        if exists:
+            return JsonResponse({'status': 221111, 'errmsg': '手机号已注册', 'key': 'phone_number'})
+
         sms_code = random.randrange(10000, 99999)
         conn = get_redis_connection()
         conn.setex(phone_number, 90, sms_code)
-        sms.send_sms_single(phone_number, template_id, [sms_code, 3])
-        return JsonResponse({'errmsg': '发送成功'})
+        response = sms.send_sms_single(phone_number, template_id, [sms_code, 3])
+        if response['result'] != 0:
+            return JsonResponse({'status': 216601, 'errmsg': response['errmsg'], 'key': 'code'})
+        return JsonResponse({'status': 200000, 'errmsg': '发送成功'})
